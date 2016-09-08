99ceabfd174a3d08bc9671de477c7917ac2e5e70
shway250 <jackson_duhon@yahoo.com>
2016-09-08 15:51:41 -0700

    did some stuff to nav bar...didn't really do much

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user
       session[:user_id] = user.id
-      flash[:success] = "User logged in!!"
+      flash[:success] = "#{user.name} logged in!!"
       redirect_to root_path
     else
       flash[:danger] = "Credentials Invalid!!"
