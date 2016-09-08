41612a047fd21f28539a68b0a0d3f7f40aeaee57
ChrisKenyon79 <ckenyon79@gmail.com>
2016-09-08 14:30:38 -0700

    frame for about page

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
   
   get "logout" => "sessions#destroy"
 
+  get "about" => "home#about"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
