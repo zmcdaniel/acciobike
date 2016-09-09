f58bd939150a4d2fda5c9dbd608ee6a71e5ee43d
shway250 <jackson_duhon@yahoo.com>
2016-09-09 15:03:44 -0700

    new bike form done, working otu the kinks of making new bike.

--- app/assets/javascripts/bikeMenu.js
+++
@@ -1,7 +0,0 @@
-$(".false_clicked").on("click", function(){
-  console.log("############ YAAAAAAHH!!!! IN THE JQUERY!!!!!")
-  $(".stolen_form").css("display", "inline");
-});
-$(".true_clicked").on("click", function(){
-  $(".stolen_form").css("display", "none");
-});
